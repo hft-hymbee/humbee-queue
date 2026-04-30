33753f1d010d9b27d6eddf9b27c1c31a895c7645
MuKuSh <81867465+MuKuSh11@users.noreply.github.com>
2026-04-30 14:58:18 +0530

    removed app/notification_email app/notification_sms app/logic/notification as tighlty coupled

--- app/logic/notification/dtos/notification_dtos.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-
-class SendEmailNotificationDTO(BaseModel):
-    recipient_email: str
-    template_id: str
-    subject: str
-    payload: Optional[dict] = None
