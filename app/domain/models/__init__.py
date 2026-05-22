289e24bdf89e6f2a2690ba5a03d6c4bcc184a439
MuKuSh <81867465+MuKuSh11@users.noreply.github.com>
2026-05-22 08:11:57 +0530

    Update models __init__.py to import WhatsAppTemplate model

--- app/domain/models/__init__.py
+++ app/domain/models/__init__.py
@@ -1,4 +1,5 @@
 from .notification import NotificationHistory
 from .sms_template import SMSTemplate
+from .whatsapp_template import WhatsAppTemplate
 
-__all__ = ["NotificationHistory", "SMSTemplate"]
+__all__ = ["NotificationHistory", "SMSTemplate", "WhatsAppTemplate"]
