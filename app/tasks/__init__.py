4b3eefcadff6946679d744f4b4f743fe81a2bb74
MuKuSh <81867465+MuKuSh11@users.noreply.github.com>
2026-05-27 10:27:58 +0530

    moved tasks/inapp to tasks/push

--- app/tasks/__init__.py
+++ app/tasks/__init__.py
@@ -8,4 +8,4 @@ Import all task modules here for auto-discovery.
 from tasks.email_task import send_email_notification
 from tasks.sms_task import send_sms_notification
 from tasks.whatsapp_task import send_whatsapp_notification
-from tasks.inapp_task import send_inapp_notification
+from tasks.push_task import send_push_notification
