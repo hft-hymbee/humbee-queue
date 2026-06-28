7dbceca9120ba7b853dfb00ad6544ecfb79decde
Felix Ferrao <felixferrao2.ff2@gmail.com>
2026-06-28 17:53:28 +0530

    Add EmailTemplate model for managing email templates in the database

--- app/domain/models/__init__.py
+++ app/domain/models/__init__.py
@@ -1,5 +1,6 @@
 from .notification import NotificationHistory
 from .sms_template import SMSTemplate
 from .whatsapp_template import WhatsAppTemplate
+from .email_template import EmailTemplate
 
-__all__ = ["NotificationHistory", "SMSTemplate", "WhatsAppTemplate"]
+__all__ = ["NotificationHistory", "SMSTemplate", "WhatsAppTemplate", "EmailTemplate"]
