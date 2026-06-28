d5c8f5f10ee1dc1e27ba846164d0a7ad39759dc8
MuKuSh <81867465+MuKuSh11@users.noreply.github.com>
2026-06-28 09:11:28 +0530

    Added push_delivery model to models __init__

--- app/domain/models/__init__.py
+++ app/domain/models/__init__.py
@@ -1,5 +1,6 @@
 from .notification import NotificationHistory
 from .sms_template import SMSTemplate
 from .whatsapp_template import WhatsAppTemplate
+from .push_delivery import PushDeliveryAttempt
 
-__all__ = ["NotificationHistory", "SMSTemplate", "WhatsAppTemplate"]
+__all__ = ["NotificationHistory", "SMSTemplate", "WhatsAppTemplate", "PushDeliveryAttempt"]
